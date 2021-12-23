8b78a8ebcb2509051fc6ec856c1c26d200c08ef0
alexis mateo <boku-heromateo@hotmail.com>
2021-12-22 22:41:25 -0600

    eva 1-extra

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+import axios from "axios"
+
+export async function envAPI(url){
+  try{
+    const response = await axios({
+      url: `https://myheroacademiaapi.com/api/${url}`,
+      method: `GET`,
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
 function App() {
+const[dataUwabami, setDataUwabami] = useState(null);
+const[Personajes, setPersonajes] = useState([]);
+
+  const response = async (name) => envAPI(name);
+  useEffect(() => {
+    async function loadapi() {
+      const resp = await response ("character");
+      const data = resp?.data;
+      setDataUwabami(data);
+    }
+    loadapi();
+    return function cleanup() {
+      loadapi();
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(dataUwabami);
+     if (dataUwabami !== null) {
+     setPersonajes(dataUwabami.result);
+     console.log(dataUwabami?.result[0].image);
+    }
+  }, [dataUwabami]);
+
+  useEffect(() => {
+    console.log(Personajes)
+  }, [Personajes]);
+
+  const valor = 2;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {Personajes.length !== 0 &&
+          Personajes.map((item, index) => {
+            console.log(item)
+            return (
+              <div key={index} className="card">;
+              <img src={item.image} className="App-logo" alt="logo" />
+                <div className="info">
+                  <p>{item.id}</p>
+                  <p>{item.name}</p>
+                  <p>Cumple: {item.birthday}</p>
+                  <p>occupation: {item.birthday}</p>
+                  <p>tipo de sangre: {item.bloodtype}</p>
+                  <p>Estado: {item.status}</p>
+                  <p>Equipo: {item.teams}</p>
+                  <p>alias: {item.alias}</p>
+                  </div>
+              </div>
+            );
+        })}
+        
       </header>
     </div>
   );
